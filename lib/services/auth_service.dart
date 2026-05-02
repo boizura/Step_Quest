edaae7aeea7876d606edb285a624a07d144b2165
Maliyah Fleming <liyahfleming6404@gmail.com>
2026-05-02 10:08:20 -0400

    chore: add a blank line for improved code readability in auth_service.dart

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _db = FirebaseFirestore.instance;
